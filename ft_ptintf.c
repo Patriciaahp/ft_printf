b8d596fdde7c96cc500eeeae2ba753ce2d9a052b
Patriciaahp <patrihernandezperez9@gmail.com>
2025-02-14 06:56:45 +0100

    varios parametros

--- ft_ptintf.c
+++ ft_ptintf.c
@@ -10,28 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <unistd.h>
 #include <stdarg.h>
 
-int	ft_strcmp(const char *s1, const char *s2)
+/*int ft_strcmp(const char *s1, const char *s2)
 {
-	unsigned int	i;
+	unsigned int i;
 
 	i = 0;
 
-  while (s1[i] && s2[i] && s1[i] == s2[i])
-    {
-        i++;
-    }
-    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
-int ft_to_string (const char *str)
+int ft_to_string(const char *str)
 {
-if (ft_strcmp(str, "%c") == 0)
+	if (ft_strcmp(str, "%c") == 0)
 		return 1;
-    else if (ft_strcmp(str, "%s") == 0)
+	else if (ft_strcmp(str, "%s") == 0)
 		return 2;
 	else if (ft_strcmp(str, "%p") == 0)
 		return 3;
@@ -45,20 +44,20 @@ if (ft_strcmp(str, "%c") == 0)
 		return 7;
 	else if (ft_strcmp(str, "%X") == 0)
 		return 8;
-		else if (ft_strcmp(str, "%%") == 0)
+	else if (ft_strcmp(str, "%%") == 0)
 		return 9;
 	else
 		return 0;
-}
+}*/
 
-void	ft_putchar (char c)
+void ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void	ft_putstr (char *s)
+void ft_putstr(char *s)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (s[i])
@@ -67,12 +66,12 @@ void	ft_putstr (char *s)
 		i++;
 	}
 }
-void	ft_putnbr(int nb)
+void ft_putnbr(int nb)
 {
 	if (nb == -2147483648)
 	{
 		write(1, "-2147483648", 11);
-		return ;
+		return;
 	}
 	if (nb < 0)
 	{
@@ -84,74 +83,65 @@ void	ft_putnbr(int nb)
 		ft_putnbr(nb / 10);
 		nb = nb % 10;
 	}
-	ft_putchar (nb + '0');
+	ft_putchar(nb + '0');
 }
 
-int ft_printf (char *b, ...)
+int ft_printf(char *str, ...)
 {
-	int	i;
-	int	type;
-	int	size;
+	int i;
+	int type;
+	int size;
 	va_list args;
-	va_start(args, b);
-	
+	va_start(args, str);
+
 	i = 0;
 	type = 0;
 	size = 0;
-	while (b[i])
+	while (str[i])
 	{
-		if (b[i] == '%')
+		if (str[i] == '%' && str[i + 1])
 		{
-			type = ft_to_string(b);
 			i++;
+			switch (str[i])
+			{
+			case 'c':
+				ft_putchar(va_arg(args, int));
+				size++;
+				break;
+			case 's':
+				ft_putstr(va_arg(args, char *));
+				break;
+			case 'd':
+			case 'i':
+				ft_putnbr(va_arg(args, int));
+				break;
+			case '%':
+				ft_putchar('%');
+				size++;
+				break;
+			default:
+				ft_putchar('%');
+				ft_putchar(str[i]);
+				size += 2;
+				break;
+			}
 		}
-		else 
+		else
 		{
-			ft_putchar(b[i]);
-			i++;
+			ft_putchar(str[i]);
+			size++;
 		}
-	
-		return size;
+		i++;
 	}
-switch (type)
-{
 
-case 1:
-ft_putchar(va_arg(args, int));
-	break;
-case 2:
-ft_putstr(va_arg(args, char *));
-	break;
-case 5:
-ft_putnbr(va_arg(args, int));
-default:
-	break;
-	case 9:
-	ft_putchar(va_arg(args, int));
-	break;
-}
-
-va_end(args);
+	va_end(args);
+	return (size);
 }
 
 #include <stdio.h>
 
-int main (void)
-{	
-//	ft_printf("%s", "ch");
-//	ft_printf("%c", ' ');
-//	ft_printf("%c", 'c');
-//	ft_printf("%c", ' ');
-//	ft_printf("%i", 44);
-//	ft_printf("%c", ' ');
-	ft_printf("hola %s", "ch");
-	printf("\n");
-//	printf("%s", "ch");
-//	printf("%c", ' ');
-//	printf("%c", 'c');
-//	printf("%c", ' ');
-//	printf("%i", 44);
-//	printf("%c", ' ');
-	printf("\n");
-	printf("hola %s %c", "ch", 'c');
+int main(void)
+{
+	ft_printf("Hola %s %c %i %%\n", "mundo", '!', 42);
+	printf("Hola %s %c %i %%\n", "mundo", '!', 42);
 }

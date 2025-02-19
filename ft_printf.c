cfe33eb50e8c9311a1c3e695abed89c174ac99f0
Patricia Hernandez Perez <pahernan@c3r4s4.42malaga.com>
2025-02-19 10:23:05 +0100

    refactorizado

--- ft_printf.c
+++ ft_printf.c
@@ -1,95 +1,104 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ptintf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pahernan <pahernan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 08:36:32 by pahernan          #+#    #+#             */
-/*   Updated: 2025/02/12 12:35:01 by pahernan         ###   ########.fr       */
+/*   Updated: 2025/02/19 10:22:20 by pahernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void ft_putchar(char c)
+int	ft_putchar(char c, int size)
 {
 	write(1, &c, 1);
+	size++;
+	return (size);
 }
 
-void ft_putstr(char *s)
+int	ft_putstr(char *s, int size)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (s[i])
 	{
 		write(1, &s[i], 1);
 		i++;
+		size++;
 	}
+	return (size);
 }
-void ft_putnbr(int nb)
+
+int	ft_putnbr(int nb, int size)
 {
 	if (nb == -2147483648)
 	{
 		write(1, "-2147483648", 11);
-		return;
+		size = 11;
+		return (size);
 	}
 	if (nb < 0)
 	{
-		ft_putchar('-');
+		ft_putchar('-', size);
 		nb = -nb;
 	}
 	if (nb >= 10)
 	{
-		ft_putnbr(nb / 10);
+		ft_putnbr(nb / 10, size);
 		nb = nb % 10;
 	}
-	ft_putchar(nb + '0');
+	ft_putchar(nb + '0', size);
+	return (size);
 }
 
-void ft_unsignedputnbr(unsigned int nb)
+int	ft_unsignedputnbr(unsigned int nb, int size)
 {
 	if (nb >= 10)
 	{
-		ft_unsignedputnbr(nb / 10);
+		ft_unsignedputnbr(nb / 10, size);
 		nb = nb % 10;
 	}
-	ft_putchar(nb + '0');
+	ft_putchar(nb + '0', size++);
+	return (size);
 }
 
-void ft_puthex(unsigned long n, char *hexChars)
+int	ft_puthex(unsigned long n, char *hexChars, int size)
 {
-
 	if (n >= 16)
 	{
-		ft_puthex(n / 16, hexChars);
+		ft_puthex(n / 16, hexChars, size);
 		n = n % 16;
 	}
-
-	ft_putchar(hexChars[n]);
+	ft_putchar(hexChars[n], size);
+	return (size);
 }
 
-void ft_putpointer(void *ptr)
+int	ft_putpointer(void *ptr, int size)
 {
-	unsigned long n;
+	unsigned long	n;
+
 	n = (unsigned long)ptr;
-	ft_putstr("0x");
+	ft_putstr("0x", size);
 	if (n == 0)
 	{
-		ft_putchar('0');
-		return;
+		ft_putchar('0', size);
+		return (size);
 	}
-	ft_puthex(n, "0123456789abcdef");
+	ft_puthex(n, "0123456789abcdef", size);
+	return (size);
 }
 
-int ft_printf(const char *str, ...)
+int	ft_printf(const char *str, ...)
 {
-	int i;
-	int size;
-	va_list args;
-	va_start(args, str);
+	int			i;
+	int			size;
+	va_list		args;
 
+	va_start(args, str);
 	i = 0;
 	size = 0;
 	while (str[i])
@@ -97,54 +106,36 @@ int ft_printf(const char *str, ...)
 		if (str[i] == '%' && str[i + 1])
 		{
 			i++;
-			switch (str[i])
+			if (str[i] == 'c')
+				ft_putchar(va_arg(args, int), size);
+			else if (str[i] == 's')
+				ft_putstr(va_arg(args, char *), size);
+			else if (str[i] == 'i')
+				ft_putnbr(va_arg(args, int), size);
+			else if (str[i] == 'd')
+				ft_putnbr(va_arg(args, int), size);
+			else if (str[i] == '%')
+				ft_putchar('%', size);
+			else if (str[i] == 'u')
+				ft_unsignedputnbr(va_arg(args, unsigned int), size);
+			else if (str[i] == 'x')
+				ft_puthex(va_arg(args, unsigned int), "0123456789abcdef", size);
+			else if (str[i] == 'X')
+				ft_puthex(va_arg(args, unsigned int), "0123456789ABCDEF", size);
+			else if (str[i] == 'p')
+				ft_putpointer(va_arg(args, void *), size);
+			else
 			{
-			case 'c':
-				ft_putchar(va_arg(args, int));
-				size++;
-				break;
-			case 's':
-				ft_putstr(va_arg(args, char *));
-				break;
-			case 'd':
-			case 'i':
-				ft_putnbr(va_arg(args, int));
-				break;
-			case '%':
-				ft_putchar('%');
-				size++;
-				break;
-			case 'u':
-				ft_unsignedputnbr(va_arg(args, unsigned int));
-				size++;
-				break;
-			case 'x':
-				ft_puthex(va_arg(args, unsigned int), "0123456789abcdef");
-				size++;
-				break;
-			case 'X':
-				ft_puthex(va_arg(args, unsigned int), "0123456789ABCDEF");
-				size++;
-				break;
-			case 'p':
-				ft_putpointer(va_arg(args, void *));
-				size++;
-				break;
-			default:
-				ft_putchar('%');
-				ft_putchar(str[i]);
-				size += 2;
-				break;
+				ft_putchar('%', size);
+				ft_putchar(str[i], size);
 			}
 		}
 		else
 		{
-			ft_putchar(str[i]);
-			size++;
+			ft_putchar(str[i], size);
 		}
 		i++;
 	}
-
 	va_end(args);
 	return (size);
 }
@@ -153,9 +144,10 @@ int ft_printf(const char *str, ...)
 
 int main(void)
 {
-	int num = 42;
+	int num = 2;
 	int *ptr = &num;
+	char *str = "mundo";
 
-	ft_printf("Hola %s %c %i %% %u %p %X %x\n", "ft_mundo", '!', -42, -42, ptr, num, num);
-	printf("Hola %s %c %i %% %u %p %X %x\n", "mundo", '!', -42, -42, ptr, num, num);
+	ft_printf("Hola %s %c %i %% %u %p %X %x %d\n", str, '!', num, num, ptr, num, num, num);
+	printf("Hola %s %c %i %% %u %p %X %x\n %d", str, '!', num, num, ptr, num, num, num);
 }*/
--- ft_printf.h
+++ ft_printf.h
@@ -1,9 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pahernan <pahernan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 09:14:01 by pahernan          #+#    #+#             */
+/*   Updated: 2025/02/19 09:16:44 by pahernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <unistd.h>
+# define FT_PRINTF_H
 
-int ft_printf(const char *str, ...);
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *str, ...);
 
 #endif
